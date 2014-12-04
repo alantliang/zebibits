ec7557c43b78eee5e6b23f41307c9c803a38cda2
Alan Liang <alantliang@gmail.com>
2014-12-03 23:16:53 -0500

    Adding tip calculator project

---
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Alan Liang on 12/3/14.
+//  Copyright (c) 2014 fsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
