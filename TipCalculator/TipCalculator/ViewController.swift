9f023b1235935ddf98623043fe39884dca1e7be0
Alan Liang <alantliang@gmail.com>
2014-12-08 22:14:13 -0500

    Attempt to fix view...
    View is all wonky. Need to find out how to make it look nice

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,12 +27,39 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTapped(sender : AnyObject) {
+        // 1
+        tipCalc.total = Double((totalTextField.text as NSString).doubleValue)
+        // 2
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        // 3
+        for (tipPct, tipValue) in possibleTips {
+            // 4
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        // 5
+        resultsTextView.text = results
     }
     @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
     }
     @IBAction func viewTapped(sender : AnyObject) {
+        totalTextField.resignFirstResponder()
     }
 
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        // 1
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        // 2
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        // 3
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        // 4
+        resultsTextView.text = ""
+    }
 
 }
 
