34da5c970ad2d5ab1a98e5b1018c6509fbb69fe4
Alan Liang <alantliang@gmail.com>
2015-05-09 14:31:46 -0400

    Changed port number

--- WebServer/app.js
+++ WebServer/app.js
@@ -43,6 +43,6 @@ express.compiler.compilers.less.compile = function(str, fn){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(1337, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
