b6626e1cf016897575e5d10091676634ab57a236
Alan Liang <alantliang@gmail.com>
2015-05-10 22:51:49 -0400

    Change port number to 80

--- WebServer/app.js
+++ WebServer/app.js
@@ -43,6 +43,6 @@ express.compiler.compilers.less.compile = function(str, fn){
 
 app.get('/', routes.index);
 
-app.listen(1337, function(){
+app.listen(80, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
