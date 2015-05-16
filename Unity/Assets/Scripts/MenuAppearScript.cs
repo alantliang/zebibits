4b30591d27b3235cd2da80cf8a9e6507f003cd67
Alan Liang <alantliang@gmail.com>
2015-05-15 23:27:58 -0400

    Added Building UI
    Added buttons to the build UI. Currently they don't do anything.

---
+++ Unity/Assets/Scripts/MenuAppearScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAppearScript : MonoBehaviour
+{
+	
+	public GameObject menu; // Assign in inspector
+	private bool isShowing = false;
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown ("escape")) {
+			isShowing = !isShowing;
+			menu.SetActive (isShowing);
+		}
+	}
+	
+	public void SetIsShowing (bool value)
+	{
+		isShowing = value;
+		menu.SetActive (isShowing);
+	}
+}
