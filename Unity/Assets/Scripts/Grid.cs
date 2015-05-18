ccbb02f08373ec59b502c38388de2783f7a27b21
Alan Liang <alantliang@gmail.com>
2015-05-17 23:06:07 -0400

    Allow for touch zoom and movement

---
+++ Unity/Assets/Scripts/Grid.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+	public GameObject plane;
+	public int width = 10;
+	public int height = 10;
+	
+	public GameObject[,] grid = new GameObject[10, 10];
+	
+	void Awake ()
+	{
+		grid = new GameObject[width, height];
+		// our grid is in the x/y plane
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				GameObject gridPlane = (GameObject)Instantiate (plane);
+				gridPlane.transform.position = new Vector3 (gridPlane.transform.position.x + x,
+				gridPlane.transform.position.y + y, gridPlane.transform.position.z);
+				grid [x, y] = gridPlane;
+			}
+		}
+	}
+	
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Unity/Assets/Scripts/TouchMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchMove : MonoBehaviour
+{
+	public float speed = 0.5F;
+	void Update ()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+			transform.Translate (-touchDeltaPosition.x * speed * Time.deltaTime, -touchDeltaPosition.y * speed * Time.deltaTime, 0);
+		}
+	}
+}
