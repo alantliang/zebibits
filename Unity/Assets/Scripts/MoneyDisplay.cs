42ece6def3c8fd22e5d7fe48a0b18bfa596ec3b2
Alan Liang <alantliang@gmail.com>
2015-05-19 22:22:59 -0400

    Save money to json file
    Added the ability to save money. Currently there is an add money button.

---
+++ Unity/Assets/Scripts/MoneyDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyDisplay : MonoBehaviour
+{	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// this should listen to a OnMoneyChangedEvent
+		GameObject playerWallet = GameObject.Find ("PlayerWallet");
+		PlayerWallet playerWalletScript = (PlayerWallet)playerWallet.GetComponent ("PlayerWallet");
+		TextMesh myTextMesh = (TextMesh)GetComponent ("TextMesh");
+		myTextMesh.text = "MONEY: " + playerWalletScript.Money.ToString ();
+	}
+}
---
+++ Unity/Assets/Scripts/PlayerWallet.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System.IO;
+using System;
+
+public class PlayerWallet : MonoBehaviour
+{
+	private JSONNode node;
+	private int money;
+	public int Money {
+		get { return money; }
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		string jsonStream = "";
+		using (StreamReader sr = new StreamReader(GetPlayerStatsFilepath ())) {
+			string line;
+			while ((line = sr.ReadLine()) != null) {
+				jsonStream += line;
+			}
+		}
+		
+		node = JSON.Parse (jsonStream);
+		money = Convert.ToInt32 (node ["money"]);
+	}
+	
+	string GetPlayerStatsFilepath ()
+	{
+		return Path.GetFullPath (Path.Combine (Application.streamingAssetsPath, "PlayerStats.json"));
+	}
+	
+	public void AddMoney (int amt)
+	{
+		money += amt;
+		node ["money"] = money.ToString ();
+		SaveMoney (money);
+	}
+	
+	void SaveMoney (int amt)
+	{
+		File.WriteAllText (GetPlayerStatsFilepath (), node.ToString ());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
